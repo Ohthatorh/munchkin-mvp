03a861efce20dd4100760e3c185bb77c6445a0e0
Orh <ohthatorh@yandex.ru>
2026-01-11 20:22:17 +0300

    bot token

--- backend/src/bot.ts
+++ backend/src/bot.ts
@@ -2,7 +2,8 @@ import { Telegraf } from "telegraf";
 import { addPlayer, updatePlayer, Player } from "./rooms";
 import { redis } from "./redisClient";
 
-const BOT_TOKEN = process.env.BOT_TOKEN || "<YOUR_BOT_TOKEN>";
+const BOT_TOKEN =
+  process.env.BOT_TOKEN || "8584564268:AAHVy_qrtYx5mdizX-6WzodNwFC9TguiHlY";
 const bot = new Telegraf(BOT_TOKEN);
 
 // командой /join ABC123 игрок заходит
