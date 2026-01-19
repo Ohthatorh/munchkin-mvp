a65b70064ffe5788fc850513ee0a27e7b1ffba27
Orh <ohthatorh@yandex.ru>
2026-01-20 00:29:05 +0300

    123

--- frontend/app/[roomId]/page.tsx
+++ frontend/app/[roomId]/page.tsx
@@ -12,6 +12,12 @@ interface Player {
   sex: string;
 }
 
+interface IRoomEvent {
+  timestamp: number;
+  playerId: string;
+  text: string;
+}
+
 export default function Room() {
   const params = useParams();
   const roomId = params.roomId;
@@ -19,6 +25,7 @@ export default function Room() {
   const [players, setPlayers] = useState<Player[]>([]);
   const [cube, setCube] = useState<(Player & { cube: string }) | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [history, setHistory] = useState<IRoomEvent[] | null>(null);
   const wsRef = useRef<WebSocket | null>(null);
 
   useEffect(() => {
@@ -39,6 +46,11 @@ export default function Room() {
       if (msg.type === "ERROR") setError(msg.data);
       if (msg.type === "ROOM_STATE") setPlayers(msg.data);
       if (msg.type === "GET_CUBE") setCube(msg.data);
+      if (msg.type === "ROOM_HISTORY") setHistory(msg.data);
+      if (msg.type === "ROOM_EVENT") {
+        if (!msg.data) return;
+        setHistory((prev) => [msg.data, ...prev!]);
+      }
     };
 
     return () => {
@@ -46,36 +58,77 @@ export default function Room() {
       wsRef.current = null;
     };
   }, [roomId]);
+
   if (isLoading) return <h1>Загрузка...</h1>;
   return (
     <div style={{ padding: 20 }}>
       {!error ? (
         <>
           <h1>Комната {roomId}</h1>
-          <table
-            style={{ width: "50%", marginTop: 20, borderCollapse: "collapse" }}
-          >
-            <thead>
-              <tr>
-                <th>Ник</th>
-                <th>Пол</th>
-                <th>Уровень</th>
-                <th>Шмотки</th>
-                <th>Общий урон</th>
-              </tr>
-            </thead>
-            <tbody>
-              {players.map((p) => (
-                <tr key={p.id}>
-                  <td>{p.nickname}</td>
-                  <td>{p.sex}</td>
-                  <td>{p.level}</td>
-                  <td>{p.damage}</td>
-                  <td>{p.damage + p.level}</td>
+          <div>
+            <table
+              style={{
+                width: "50%",
+                marginTop: 20,
+                borderCollapse: "collapse",
+              }}
+            >
+              <thead>
+                <tr>
+                  <th>Ник</th>
+                  <th>Пол</th>
+                  <th>Уровень</th>
+                  <th>Шмотки</th>
+                  <th>Общий урон</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {players.map((p) => (
+                  <tr key={p.id}>
+                    <td>{p.nickname}</td>
+                    <td>{p.sex}</td>
+                    <td>{p.level}</td>
+                    <td>{p.damage}</td>
+                    <td>{p.damage + p.level}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            {history && (
+              <table
+                style={{
+                  width: "50%",
+                  marginTop: 20,
+                  borderCollapse: "collapse",
+                }}
+              >
+                <thead>
+                  <tr>
+                    <th>Время</th>
+                    <th>Событие</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {history.map((p) => (
+                    <tr key={p.timestamp}>
+                      <td>
+                        {new Date(p.timestamp).toLocaleString("ru-RU", {
+                          timeZone: "Europe/Moscow",
+                          year: "numeric",
+                          month: "2-digit",
+                          day: "2-digit",
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          second: "2-digit",
+                        })}
+                      </td>
+                      <td>{p.text}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
           {cube?.cube && (
             <p>
               Игрок {cube?.nickname} сделал бросок кубика, результат:{" "}
