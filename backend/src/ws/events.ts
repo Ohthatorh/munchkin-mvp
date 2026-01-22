6d3eb39a6ae2fc26256c67b4bef2476b353098bc
Orh <ohthatorh@yandex.ru>
2026-01-22 23:38:38 +0300

    123

--- backend/src/ws/events.ts
+++ backend/src/ws/events.ts
@@ -1,55 +1,57 @@
-import { wss } from ".";
+import { WebSocketServer } from "ws";
 import { getRoomHistory, roomExists } from "../redis/helpers";
 import { IRoomEvent, WSMessage } from "../types";
 import { broadcastRoomState } from "./broadcasts";
 import { wsRooms } from "./wsRooms";
 
-wss.on("connection", (ws, req) => {
-  let currentRoom: string | null = null;
+export function registerWsEvents(wss: WebSocketServer) {
+  wss.on("connection", (ws, req) => {
+    let currentRoom: string | null = null;
 
-  ws.on("message", async (message) => {
-    try {
-      const msg: WSMessage = JSON.parse(message.toString());
-      const { type, data } = msg;
+    ws.on("message", async (message) => {
+      try {
+        const msg: WSMessage = JSON.parse(message.toString());
+        const { type, data } = msg;
 
-      switch (type) {
-        case "JOIN_ROOM":
-          const roomCode = data.roomCode;
-          if (!roomCode) return;
-          const exists = await roomExists(roomCode);
-          if (!exists) {
-            ws.send(
-              JSON.stringify({
-                type: "ERROR",
-                data: `Комната ${roomCode} не существует ❌`,
-              }),
-            );
-            return;
-          }
+        switch (type) {
+          case "JOIN_ROOM":
+            const roomCode = data.roomCode;
+            if (!roomCode) return;
+            const exists = await roomExists(roomCode);
+            if (!exists) {
+              ws.send(
+                JSON.stringify({
+                  type: "ERROR",
+                  data: `Комната ${roomCode} не существует ❌`,
+                }),
+              );
+              return;
+            }
 
-          currentRoom = roomCode;
+            currentRoom = roomCode;
 
-          if (!wsRooms[currentRoom!]) wsRooms[currentRoom!] = new Set();
-          wsRooms[currentRoom!].add(ws);
-          const history: IRoomEvent[] = await getRoomHistory(currentRoom!);
-          ws.send(JSON.stringify({ type: "ROOM_HISTORY", data: history }));
-          await broadcastRoomState(currentRoom!);
-          break;
+            if (!wsRooms[currentRoom!]) wsRooms[currentRoom!] = new Set();
+            wsRooms[currentRoom!].add(ws);
+            const history: IRoomEvent[] = await getRoomHistory(currentRoom!);
+            ws.send(JSON.stringify({ type: "ROOM_HISTORY", data: history }));
+            await broadcastRoomState(currentRoom!);
+            break;
 
-        default:
-          ws.send(JSON.stringify({ type: "ERROR", data: "Unknown type" }));
+          default:
+            ws.send(JSON.stringify({ type: "ERROR", data: "Unknown type" }));
+        }
+      } catch (err: any) {
+        ws.send(JSON.stringify({ type: "ERROR", data: err.message }));
       }
-    } catch (err: any) {
-      ws.send(JSON.stringify({ type: "ERROR", data: err.message }));
-    }
-  });
+    });
 
-  ws.on("close", () => {
-    if (currentRoom && wsRooms[currentRoom]) {
-      wsRooms[currentRoom].delete(ws);
-      if (wsRooms[currentRoom].size === 0) {
-        delete wsRooms[currentRoom];
+    ws.on("close", () => {
+      if (currentRoom && wsRooms[currentRoom]) {
+        wsRooms[currentRoom].delete(ws);
+        if (wsRooms[currentRoom].size === 0) {
+          delete wsRooms[currentRoom];
+        }
       }
-    }
+    });
   });
-});
+}
--- backend/src/ws/index.ts
+++ backend/src/ws/index.ts
@@ -1,60 +1,9 @@
-import { WebSocket, WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
 import { httpServer } from "../server";
-import { broadcastRoomState } from "./broadcasts";
-import { IRoomEvent, WSMessage } from "../types";
-import { getRoomHistory, roomExists } from "../redis/helpers";
-import { wsRooms } from "./wsRooms";
+import { registerWsEvents } from "./events";
 
 export const wss = new WebSocketServer({ server: httpServer });
 
-wss.on("connection", (ws, req) => {
-  let currentRoom: string | null = null;
-
-  ws.on("message", async (message) => {
-    try {
-      const msg: WSMessage = JSON.parse(message.toString());
-      const { type, data } = msg;
-
-      switch (type) {
-        case "JOIN_ROOM":
-          const roomCode = data.roomCode;
-          if (!roomCode) return;
-          const exists = await roomExists(roomCode);
-          if (!exists) {
-            ws.send(
-              JSON.stringify({
-                type: "ERROR",
-                data: `Комната ${roomCode} не существует ❌`,
-              }),
-            );
-            return;
-          }
-
-          currentRoom = roomCode;
-
-          if (!wsRooms[currentRoom!]) wsRooms[currentRoom!] = new Set();
-          wsRooms[currentRoom!].add(ws);
-          const history: IRoomEvent[] = await getRoomHistory(currentRoom!);
-          ws.send(JSON.stringify({ type: "ROOM_HISTORY", data: history }));
-          await broadcastRoomState(currentRoom!);
-          break;
-
-        default:
-          ws.send(JSON.stringify({ type: "ERROR", data: "Unknown type" }));
-      }
-    } catch (err: any) {
-      ws.send(JSON.stringify({ type: "ERROR", data: err.message }));
-    }
-  });
-
-  ws.on("close", () => {
-    if (currentRoom && wsRooms[currentRoom]) {
-      wsRooms[currentRoom].delete(ws);
-      if (wsRooms[currentRoom].size === 0) {
-        delete wsRooms[currentRoom];
-      }
-    }
-  });
-});
+registerWsEvents(wss);
 
 console.log("WebSocket server started 🚀");
